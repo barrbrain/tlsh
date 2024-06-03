1d560e0a9e2a3ac26643dad78b944ff44b713cc5
David Michael Barr <b@rr-dav.id.au>
2024-06-03 20:55:16 +0900

    Add `get_tertiles` method under `tlshx` feature

--- src/quantile.rs
+++ src/quantile.rs
@@ -103,6 +103,75 @@ pub fn get_quartiles<const EFF_BUCKETS: usize>(bucket: &[u32; BUCKETS]) -> (u32,
     (q1, q2, q3)
 }
 
+#[allow(dead_code)]
+#[cfg(feature = "tlshx")]
+pub fn get_tertiles<const EFF_BUCKETS: usize>(bucket: &[u32; BUCKETS]) -> (u32, u32) {
+    let mut short_cut_left = [0; EFF_BUCKETS];
+    let p1 = EFF_BUCKETS / 3 - 1;
+    let p2 = EFF_BUCKETS - EFF_BUCKETS / 3 - 1;
+    let end = EFF_BUCKETS - 1;
+    let mut q1 = 0;
+    let q2;
+
+    // Safety: this expect is eliminated at compile time, as the compiler can
+    // trivially verify that EFF_BUCKETS <= BUCKETS.
+    let mut bucket_copy: [u32; EFF_BUCKETS] = bucket[..EFF_BUCKETS]
+        .try_into()
+        .expect("EFF_BUCKETS is bigger than BUCKETS");
+
+    let mut spl = 0;
+    let mut l = 0;
+    let mut r = end;
+    loop {
+        let ret = partition(&mut bucket_copy, l, r);
+        match ret.cmp(&p2) {
+            Ordering::Greater => {
+                r = ret - 1;
+            }
+            Ordering::Less => {
+                l = ret + 1;
+                short_cut_left[spl] = ret;
+                spl += 1;
+            }
+            Ordering::Equal => {
+                q2 = bucket_copy[p2];
+                break;
+            }
+        }
+    }
+
+    short_cut_left[spl] = p2 - 1;
+
+    let mut l = 0;
+    for mut r in short_cut_left.iter().take(spl + 1).copied() {
+        match r.cmp(&p1) {
+            Ordering::Greater => {
+                loop {
+                    let ret = partition(&mut bucket_copy, l, r);
+                    match ret.cmp(&p1) {
+                        Ordering::Greater => r = ret - 1,
+                        Ordering::Less => l = ret + 1,
+                        Ordering::Equal => {
+                            q1 = bucket_copy[p1];
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            Ordering::Less => {
+                l = r;
+            }
+            Ordering::Equal => {
+                q1 = bucket_copy[p1];
+                break;
+            }
+        }
+    }
+
+    (q1, q2)
+}
+
 fn partition(buf: &mut [u32], left: usize, right: usize) -> usize {
     if left == right {
         return left;
